35bec099225b8ccfe39628425f91398d5be5a929
enka <1935915+enka@users.noreply.github.com>
2018-11-14 21:29:38 +0100

    Rendering multiple props into a Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './Header/Header';
 import Menu from './Menu/Menu';
+import Info from './Info/Info';
 import Footer from './Footer/Footer';
 import Loader from './Loader/Loader';
 import Notification from './Notification/Notification';
@@ -7,6 +8,7 @@ import Notification from './Notification/Notification';
 export {
     Header,
     Menu,
+    Info,
     Footer,
     Loader,
     Notification
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Fetch } from '../../services/api';
+import { Info, Notification, Loader } from '../../components';
+
+const FETCH_OPTIONS = {
+    method: 'GET',
+    headers: {}
+};
 
 class Home extends React.Component {
     render() {
         return (
             <div className="Home" location={this.props.location}>
-                Home Page
+                <div className="Home__info">
+                    <Fetch path={'general'} options={FETCH_OPTIONS}>
+                        {({ data, loading, error }) => {
+                            if (error) {
+                                return (
+                                    <Notification type="error"
+                                        message={error.message}
+                                    />
+                                );
+                            }
+                            if (loading) {
+                                return <Loader />;
+                            }
+                            if (data) {
+                                return (
+                                    <Info title="About Accenture"
+                                        data={data}
+                                    />
+                                );
+                            }
+                            return <Loader />;
+                        }}
+                    </Fetch>
+                </div>
             </div>
         );
     }
