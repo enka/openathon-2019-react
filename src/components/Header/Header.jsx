6b1ff35f3d89ab59c801855f6c0188af9d13793f
enka <1935915+enka@users.noreply.github.com>
2018-11-14 16:43:24 +0100

    Adding Router

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import propTypes from 'prop-types';
-import { Menu } from '../../components'
+import { Menu } from '../../components';
+import { Fetch } from '../../services/api';
+import { Loader, Notification } from '../../components';
 import './Header.css';
+
+const FETCH_OPTIONS = {
+    method: 'GET',
+    headers: {}
+};
 
 class Header extends React.Component {
     render() {
         return (
             <div className="Header">
                 <div className="Header__logo">
-                    <img alt="Accenture Logo"
-                        src={this.props.logo}
-                    />
+                    <Fetch path={'general'} options={FETCH_OPTIONS}>
+                        {({ data, loading, error }) => {
+                            if (error) {
+                                return <Notification type="error" message={error.message} />;
+                            }
+                            if (loading) {
+                                return <Loader />;
+                            }
+                            if (data && data.logo) {
+                                return <img alt="Accenture Logo" src={data.logo} />;
+                            }
+                            return <Loader />;
+                        }}
+                    </Fetch>
                 </div>
                 <h1 className="Header__title">II OPENATHON Custom Open Cloud</h1>
                 <Menu />
@@ -19,9 +36,4 @@ class Header extends React.Component {
     }
 }
 
-Header.propTypes = {
-    logo: propTypes.string
-}
-
 export default Header;
-
---
+++ src/containers/index.js
@@ -0,0 +1,15 @@
+import Main from './Main/Main';
+import Home from './Home/Home';
+import Services from './Services/Services';
+import Innovation from './Innovation/Innovation';
+import Guestbook from './Guestbook/Guestbook';
+import NotFound from './NotFound/NotFound';
+
+export {
+    Main,
+    Home,
+    Services,
+    Innovation,
+    Guestbook,
+    NotFound
+};
