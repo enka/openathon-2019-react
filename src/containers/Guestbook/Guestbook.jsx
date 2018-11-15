2e6de88cde84f177d04e2c445f1b64733ef285d4
enka <1935915+enka@users.noreply.github.com>
2018-11-15 12:32:44 +0100

    Creating a new Form Component

--- src/containers/Guestbook/Guestbook.jsx
+++ src/containers/Guestbook/Guestbook.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Form } from '../../components';
+import './Guestbook.css';
 
-    class Guestbook extends React.Component {
-        render() {
-            return (
-                <div className="Guestbook" location={this.props.location}>
-                    Guestbook Page
-                </div>
-            );
-        }
+class Guestbook extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            entries: []
+        };
+        this.submitForm = this.submitForm.bind(this);
     }
 
-    export default Guestbook;
+    submitForm(newEntry) {}
+
+    render() {
+        return (
+            <div className="Guestbook" location={this.props.location}>
+                <header className="Guestbook__header">
+                    <h1>Guestbook</h1>
+                </header>
+                <section className="Guestbook__content">
+                    <Form className="Guestbook__form" title="Guestbook Form" />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Guestbook;
